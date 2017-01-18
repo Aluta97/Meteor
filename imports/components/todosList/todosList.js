6741a97afdc57e64c7c22cb26218c1552653cfc9
Lusindiso <lusindiso@projectcodex.co>
2017-01-18 16:29:40 +0200

    angular

---
+++ imports/components/todosList/todosList.js
@@ -0,0 +1,49 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks.js';
+
+import template from './todosList.html';
+
+class TodosListCtrl {
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.helpers({
+      tasks() {
+        return Tasks.find({});
+      }
+    })
+  }
+}
+
+// addTask(newTask) {
+//     // Insert a task into the collection
+//     Tasks.insert({
+//       text: newTask,
+//       createdAt: new Date
+//     });
+//     // Clear form
+//     this.newTask = '';
+//   }
+// }
+//
+// this.helpers({
+//       tasks() {
+//         // Show newest tasks at the top
+//         return Tasks.find({}, {
+//           sort: {
+//             createdAt: -1
+//           }
+//         });
+//       }
+//     })
+//   }
+
+export default angular.module('todosList', [
+  angularMeteor
+])
+
+.component('todosList', {
+  templateUrl: 'imports/components/todosList/todosList.html',
+  controller: ['$scope', TodosListCtrl]
+  });
