d8fff9b1799daccd6aac8ce34b7d5a34baee3810
Lusindiso <lusindiso@projectcodex.co>
2017-01-19 16:37:51 +0200

    logins

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -8,36 +8,61 @@ class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.hideCompleted = false;
+
     this.helpers({
-      tasks() {
-        return Tasks.find({});
-      }
-    })
-  }
+     tasks() {
+
+       const selector = {};
+
+       // If hide completed is checked, filter tasks
+       if (this.getReactively('hideCompleted')) {
+         selector.checked = {
+           $ne: true
+         };
+       }
+       // Show newest tasks at the top
+       return Tasks.find({}, {
+         sort: {
+           createdAt: -1
+         }
+       });
+     }
+   })
 }
 
-// addTask(newTask) {
-//     // Insert a task into the collection
-//     Tasks.insert({
-//       text: newTask,
-//       createdAt: new Date
-//     });
-//     // Clear form
-//     this.newTask = '';
-//   }
-// }
-//
-// this.helpers({
-//       tasks() {
-//         // Show newest tasks at the top
-//         return Tasks.find({}, {
-//           sort: {
-//             createdAt: -1
-//           }
-//         });
-//       }
-//     })
-//   }
+incompleteCount() {
+       return Tasks.find({
+         checked: {
+           $ne: true
+         }
+       }).count();
+     }
+
+//funtion for adding new task
+  addTask(newTask) {
+      // Insert a task into the collection
+      Tasks.insert({
+        text: newTask,
+        createdAt: new Date
+      });
+      // Clear form
+      this.newTask = '';
+    }
+
+    setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+
+  removeTask(task) {
+    Tasks.remove(task._id);
+  }
+ }
 
 export default angular.module('todosList', [
   angularMeteor
